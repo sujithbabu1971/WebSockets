a0932785f6db2cd307911a36d2de8b8874440021
sujithbabu1971 <sujithkp@yahoo.com>
2017-08-20 11:16:30 +0530

    Mocha support added

---
+++ test/mochaTest.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+// Note that here it is should() and not just should.
+// Whereas for expect and assert styles, it is just assert and expect.
+var should = require('chai').should();
+//describe is the syntax for test suite
+describe('tests', function(){
+	// it is syntaxt for test case.
+	it('some tests', function(){
+		
+		var a='a';
+		// Will pass
+		expect(a).to.equal('a');
+		// Will pass
+		assert.equal(a,'a');
+		// Will pass
+		a.should.equal('a');
+		// Will  fail
+		expect(a).to.equal('b');
+		
+		
+		
+	});
+	
+});
